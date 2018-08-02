d6ba70ad86d28b265027293b48dfad156971a758
AJArrott <mrajarrott@gmail.com>
2018-08-01 20:38:08 -0700

    Added Snowball option to debt calculator

--- DebtCalculator/Models/Menus/DisplayPayoffMenu.cs
+++ DebtCalculator/Models/Menus/DisplayPayoffMenu.cs
@@ -21,8 +21,9 @@ namespace DebtCalculator.Models.Menus
                 Console.WriteLine("Payoff Calculator");
                 Console.WriteLine("---------------------");
                 Console.WriteLine("1: Manual Entry (Estimate Payoff)");
-                Console.WriteLine("2: Generate Payment Plan");
-                Console.WriteLine("3: Update Current Excess Income ({0:C}, {1:C} Required)", _totalIncome, DebtCollection.TotalRequiredIncome);
+                Console.WriteLine("2: Generate Payment Plan (Highest Interest per Month First)");
+                Console.WriteLine("3: Generate Payment Plan (Snowball, Lowest Balance First)");
+                Console.WriteLine("4: Update Current Excess Income ({0:C}, {1:C} Required)", _totalIncome, DebtCollection.TotalRequiredIncome);
                 Console.WriteLine("B: Go Back");
                 Console.WriteLine("---------------------");
                 Console.Write("Selection: ");
@@ -47,6 +48,11 @@ namespace DebtCalculator.Models.Menus
 
                 if (key.Key == ConsoleKey.D3)
                 {
+                    CalculateSnowballPayoff();
+                }
+
+                if (key.Key == ConsoleKey.D4)
+                {
                     do
                     {
                         Console.Write("Enter new excess income amount (e.g. 5000): ");
@@ -58,64 +64,100 @@ namespace DebtCalculator.Models.Menus
             } while (true);
         }
 
-        private static void CalculateOptimalPayoff()
+        private static bool IncomeHighEnough()
         {
-            if(_totalIncome < DebtCollection.TotalRequiredIncome)
-            {
-                Console.WriteLine("Must have at least {0:C} (current income: {1:C})", DebtCollection.TotalRequiredIncome, _totalIncome);
-                Console.Write("Press any key to continue...");
-                Console.ReadKey();
-                return;
-            }
+            if (_totalIncome >= DebtCollection.TotalRequiredIncome) return true;
+            
+            Console.WriteLine("Must have at least {0:C} (current income: {1:C})", DebtCollection.TotalRequiredIncome, _totalIncome);
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            return false;
+        }
+
+        private static void CalculateSnowballPayoff()
+        {
+            if (!IncomeHighEnough()) return;
 
             decimal allowance = _totalIncome;
             decimal extraIncome = _totalIncome - DebtCollection.TotalRequiredIncome;
+
+            // get copy of list
+            var lowestFirst = DebtCollection.GetDebts.OrderBy(x => x.GetCurrentMinimumPayment().StartingBalance).Select(x => new Debt(x.LoanName, x.Apr, x.CurrentBalance)).ToList();
+            List<Payment> allPayments = new List<Payment>();
+
+            while(lowestFirst.Any(x => x.CurrentBalance > 0.00m))
+            {
+                var pmt = PaymentLogic(lowestFirst);
+                allPayments.AddRange(pmt);
+
+                lowestFirst = lowestFirst.OrderBy(x => x.GetCurrentMinimumPayment().StartingBalance).ToList();
+            }
+
+            DisplayPayments(allPayments);
+        }
+
+        private static void CalculateOptimalPayoff()
+        {
+            if (!IncomeHighEnough()) return;
 
             var mostInterest = DebtCollection.GetDebts.OrderByDescending(x => x.GetCurrentMinimumPayment().Interest).Select(x => new Debt(x.LoanName, x.Apr, x.CurrentBalance)).ToList();
             List<Payment> allPayments = new List<Payment>();
 
             // highest will be first in list
-            while (mostInterest.Any(x => x.CurrentBalance > 1.00m))
+            while (mostInterest.Any(x => x.CurrentBalance > 0.00m))
             {
-                extraIncome = _totalIncome - (mostInterest.Sum(x => x.GetCurrentMinimumPayment().Amount));
+                var pmt = PaymentLogic(mostInterest);
+                allPayments.AddRange(pmt);
 
-                for (int i = 0; i < mostInterest.Count(); i++)
-                {
-                    decimal amountToPay = mostInterest[i].GetCurrentMinimumPayment().Amount;
-                    if(extraIncome > 0.00m && amountToPay > 0.00m)
-                    {
-                        amountToPay = amountToPay + extraIncome;
-                    }
-
-                    var payment = mostInterest[i].MakeSinglePayment(amountToPay);
-
-                    if(payment.Amount < (amountToPay))
-                    {
-                        extraIncome = (amountToPay - payment.Amount);
-                    }
-                    else
-                    {
-                        extraIncome = 0.00m;
-                    }
-
-                    // don't add 0 payment
-                    if (payment.Amount == 0.00m) continue;
-
-                    allPayments.Add(payment);
-                }
-
-                var db = mostInterest.OrderByDescending(x => x.GetCurrentMinimumPayment().Interest).Select(x => x.GetCurrentMinimumPayment().Interest).ToList();
                 mostInterest = mostInterest.OrderByDescending(x => x.GetCurrentMinimumPayment().Interest).ToList();
             }
 
+            DisplayPayments(allPayments);
+        }
 
-            foreach(var pmt in allPayments)
+        private static void DisplayPayments(List<Payment> payments)
+        {
+            foreach (var pmt in payments)
             {
                 Console.WriteLine(pmt.ToString());
             }
 
             Console.Write("Press any key to continue...");
             Console.ReadKey();
+        }
+
+        private static IEnumerable<Payment> PaymentLogic(IList<Debt> debts)
+        {
+            List<Payment> currentMonthPayments = new List<Payment>();
+            decimal extraIncome = _totalIncome - (debts.Sum(x => x.GetCurrentMinimumPayment().Amount));
+            for (int i = 0; i < debts.Count(); i++)
+            {
+                decimal amountToPay = debts[i].GetCurrentMinimumPayment().Amount;
+                if (amountToPay == 0.00m) continue;
+
+                if (extraIncome > 0.00m && amountToPay > 0.00m)
+                {
+                    amountToPay = amountToPay + extraIncome;
+                }
+
+                var payment = debts[i].MakeSinglePayment(amountToPay);
+
+                if (payment.Amount < (amountToPay))
+                {
+                    extraIncome = (amountToPay - payment.Amount);
+                }
+                else
+                {
+                    extraIncome = 0.00m;
+                }
+
+                // don't add 0 payment
+                if (payment.Amount == 0.00m) continue;
+
+                currentMonthPayments.Add(payment);
+            }
+
+            return currentMonthPayments;
         }
 
         private static void CalculateManualEntry()
